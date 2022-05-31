e338ae047bc4786262c8be03ecb26ff8668ad914
Semeiah Bourn <semeiahbourn@MacBook-Pro-2.local>
2022-05-31 11:54:57 -0400

    I can view a list of all my pokemon in the browser

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require ("express");
 const app = express();
  const pokemon = require ('./models/pokemon')
 
+app.set("view engine","ejs");
+app.set ("views","./views");
 
 app.get("/", (req, res)=>{
     res.send("Welcome to the Pokemon App!");
 })
 app.get("/pokemon", (req, res)=>{
-    res.send(pokemon)
+    res.render("index", {data:pokemon, pageTitle: "All the Pokemon"});
 })
 app.listen (3000, ()=>{
     console.log (`server is running on port: ` );
