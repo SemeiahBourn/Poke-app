695279a2632b9fa548a0b3a77e49d2b01295de5d
Semeiah Bourn <semeiahbourn@MacBook-Pro-2.local>
2022-05-31 10:32:29 -0400

    My server is up and running

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
- console.log ("sam")
+const express = require ("express");
+const app = express();
+// const res = require("express/lib/response");
+// const PORT = 3000;
+
+
+app.get("/", (req, res)=>{
+    res.send("Welcome to the Pokemon App!");
+})
+app.listen (3000, ()=>{
+    console.log (`server is running on port: ` );
+})
